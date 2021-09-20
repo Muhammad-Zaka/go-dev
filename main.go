3ab1ba669dd8e42b39aacb52f208257bb163a60b
muhammad-zaka <m.zkaurrehman@gmail.com>
2021-09-20 23:20:12 +0500

    Basic Code

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Ooops", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+	http.ListenAndServe(":9090", nil)
+}
